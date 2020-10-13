d32a4daf374705ff6b99a6032c20886fcbf6e15b
Host default domain <r5dev@r5dev.xyz>
2020-10-13 09:27:13 +0000

    remove mixed content

--- push.js
+++ push.js
@@ -7,6 +7,13 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:test@code.co.uk', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {};
+let sub = {
+    "endpoint": "https://fcm.googleapis.com/fcm/send/cLbAAb4a9UY:APA91bF1rnvRcpitMWKyHvZrgLGo9_3V2YMGyoRgsCLVo85r0l_Ewd-pJqFoQ-Lkzcxksj2RGCDXugnnwc3LAyPSIRwk6Yjg7cRMNdquNeknuIujJbZiy9LXjU1vWOY2mPNi8u8zoVtM",
+    "expirationTime": null,
+    "keys": {
+        "p256dh": "BAXjlfHZ2JAN4iN5cw4KebgyT49_3xwKpZaVO9TdfNWwbsorG0FkTM9dqw9v5cun4fLRqa5C_-JLLv8zvjoX4Rc",
+        "auth": "rnfl5HiTsC6XyYbmJ-XPNQ"
+    }
+};
 
-push.sendNotifications(sub, test)
+push.sendNotifications(sub, 'test message')
